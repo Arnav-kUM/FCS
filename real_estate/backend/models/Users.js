be4f88dfe7730d335e230d0f96191d6ea1174acd
Yuganshu kajla <yuganshu20418@iiitd.ac.in>
2023-10-28 13:21:40 +0530

    user schema

---
+++ real_estate/backend/models/Users.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const {Schema}= mongoose;
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    mobile: {
+        type: Number,
+        required: true
+    } 
+});
+module.exports = mongoose.model('user', UserSchema);
