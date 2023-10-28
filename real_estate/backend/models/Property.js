0c8488962585cb0f061fff6151b911af262a73dc
mohitbansal2020526 <mohit20526@iiitd.ac.in>
2023-10-28 13:27:25 +0530

    property schema

---
+++ real_estate/backend/models/Property.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const {Schema}= mongoose;
+
+const propertySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  images: [{
+    type: String,
+  }],
+  location: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  buyer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  status: {
+    type: String,
+    enum: ['available', 'sold', 'rented'],
+    default: 'available',
+  },
+  reports: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+  dateOfListing: {
+    type: Date,
+    default: Date.now,
+  },
+  dateOfSale: {
+    type: Date,
+  },
+});
+
+const Property = mongoose.model('Property', propertySchema);
+
+module.exports = Property;
--- real_estate/backend/models/Users.js
+++ real_estate/backend/models/Users.js
@@ -4,7 +4,7 @@ const UserSchema = new Schema({
     username: {
         type: String,
         required: true
-    },
+    }, 
     email: {
         type: String,
         required: true,
