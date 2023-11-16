428c1e23dccb0f914710c619580233e0866f848d
mohitbansal2020526 <mohit20526@iiitd.ac.in>
2023-11-17 01:21:55 +0530

    changed back the last comit

--- real_estate/backend/index.js
+++ real_estate/backend/index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 
 connectToMongo();
 const app = express()
-const port = process.env.PORT || 3000
+const port = 3000
 app.use(cors())
 app.use(express.json())
 app.use('/api/auth',require('./routes/auth'))
